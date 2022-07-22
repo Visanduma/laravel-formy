14ddc5db2bdb60a778816b3fffcd713472df2601
LaHiRu <lahirulhr@gmail.com>
2022-07-22 16:22:27 +0530

    middleware config

--- config/formy.php
+++ config/formy.php
@@ -1,5 +1,11 @@
 <?php
 // config for Visanduma/LaravelFormy
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
+
 return [
+
+    'middlewares' => [
+        'web', InitializeTenancyByDomain::class
+    ]
 
 ];
